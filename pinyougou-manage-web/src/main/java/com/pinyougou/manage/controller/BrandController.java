696ae952de12ab518f9ad45009c88268df462f0e
GRui <grui233@foxmail.com>
2019-09-03 17:35:31 +0800

    第3天：品牌管理（增删改查搜索）&使用Nginx代理端口

--- pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -4,10 +4,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.github.pagehelper.PageInfo;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.pinyougou.vo.Result;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +17,7 @@ public class BrandController {
     private BrandService brandService;
 
     /**
-     *
+     *  查询所有品牌
      * @return
      */
     @GetMapping("/findAll")
@@ -59,5 +57,64 @@ public class BrandController {
     ){
         //return brandService.testPage(pageNum,pageSize);
         return brandService.findPage(pageNum,pageSize);
+    }
+
+    /**
+     * 新增品牌
+     * @param brand
+     * @return
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody TbBrand brand){
+        try {
+            brandService.add(brand);
+            return Result.ok("新增品牌成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("新增品牌失败!");
+    }
+
+    /**
+     * 根据id查询
+     * @param id 品牌id
+     * @return 品牌
+     */
+    @GetMapping("/findOne/{id}")
+    public TbBrand findOne(@PathVariable Long id){
+        return brandService.findOne(id);
+    }
+
+    /**
+     * 批量删除品牌
+     * @param ids 选中的品牌id
+     * @return 返回品牌
+     */
+    @GetMapping("/delete")
+    public Result delete(Long[] ids){
+        try {
+            brandService.deleteByIds(ids);
+            return Result.ok("品牌删除成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("品牌删除失败!");
+    }
+
+    /**
+     * 根据分页信息和查询条件分页模糊查询品牌数据
+     * @param pageNum 页号
+     * @param pageSize 页大小
+     * @param brand 查询条件
+     * @return 分页信息对象
+     */
+    @PostMapping("/search")
+    public PageInfo<TbBrand> search
+            (
+               @RequestParam(defaultValue = "1") Integer pageNum,
+               @RequestParam(defaultValue = "10") Integer pageSize,
+               @RequestBody TbBrand brand
+            ){
+        return brandService.search(pageNum,pageSize,brand);
     }
 }
---
+++ pinyougou-manage-web/src/main/webapp/js/controller/brandController.js
@@ -0,0 +1,82 @@
+//创建vue实例
+var app = new Vue({
+    el:"#app",
+    //数据
+    data:{
+        //品牌列表
+        entityList:[],
+        //总记录数
+        total:0,
+        //页数
+        pageSize:10,
+        //当前页
+        pageNum:1,
+        //实体
+        entity:{},
+        //选中的ids
+        ids:[],
+        //查询条件对象
+        searchEntity:{}
+    },
+    methods:{
+        //批量删除
+        deleteList:function () {
+            if (this.ids.length == 0){
+                alert("请选择要删除的记录");
+                return;
+            }
+            //确认是否要删除
+            if(confirm("确定要删除选中的记录吗？")){
+                axios.get("../brand/delete.do?ids=" + this.ids).then(function (response) {
+                    if(response.data.success){
+                        app.searchList(1);
+                        //清空选择的id
+                        app.ids = [];
+                    }else{
+                        alert(response.data.message);
+                    }
+                });
+            }
+        },
+        //根据id查询
+        findOne: function (id) {
+            axios.get("../brand/findOne/" + id + ".do").then(function (response) {
+                app.entity = response.data;
+            });
+        },
+        //保存数据
+        save:function(){
+            axios.post("../brand/add.do",this.entity).then(function (response) {
+                if(response.data.success){
+                    //保存成功
+                    app.searchList(1);
+                }else {
+                    //保存失败
+                    alert(response.data.message);
+                }
+            });
+        },
+        //分页查询
+        searchList:function (curPage) {
+            this.pageNum=curPage;
+            /*axios.get("../brand/findPage.do?pageNum="+this.pageNum+"&pageSize="+this.pageSize).then(function (response) {
+                app.entityList=response.data.list;
+                app.total=response.data.total;
+            });*/
+
+            axios.post("../brand/search.do?pageNum="+this.pageNum+"&pageSize="+this.pageSize,this.searchEntity).then(function (response) {
+                //记录列表
+                app.entityList=response.data.list;
+                //符合本次查询的总记录数
+                app.total=response.data.total;
+            });
+        }
+    },
+    // 传统方式写create:function(){}
+    created(){//需要打开设置 languages&frameworks--javaScript  把版本号改成ECMAScript 6
+        /*axios.get("../brand/findAll.do").then(function (response) {
+            app.entityList = response.data;
+        });*/
+        this.searchList(this.pageNum);
+    }
+});
